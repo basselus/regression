267ffeadbdd0883ae85683f442bfd89b7ce2bd49
basselus <Mozes-live@hotmail.fr>
2017-11-02 13:52:58 +0100

    debug previous commit issue

--- lm_housing.R
+++ lm_housing.R
@@ -66,6 +66,16 @@ reg<- function (x,y){
 
   }
 
+reg_2<- function (x,y){
+  
+  x<-as.matrix(x)
+  x<-cbind(Intercept=1, x)
+  b<-solve(t(x) %*% x) %*% t(x) %*% y
+  colnames(b)<- "estimate"
+  print(b)
+  
+}
+
 
 #**********************************************************
 # use the reg function to build a multiple regression model
