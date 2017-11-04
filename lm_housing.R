ac3f925c10cd8cc22fa55802584eaed72072a2e8
basselus <Mozes-live@hotmail.fr>
2017-11-05 00:12:26 +0100

    final_code

--- lm_housing.R
+++ lm_housing.R
@@ -88,6 +88,7 @@ install.packages("fastmatch")
 fmatch("lstat", names(Boston))
 fmatch("rm", names(Boston))
 
+
 library(psych)
 pairs.panels(Boston[,c(13,6,14)])
 
@@ -117,29 +118,95 @@ abline(lm.fit, col="red", lwd=3)
 #****************************************************
 
 #Multiple linear regression with all predictors
-lm.fit_1<-lm(medv~., data=Boston)
-summary(lm.fit_1)
-names(lm.fit_1)
-coef(lm.fit_1)
+lm.fit_all<-lm(medv~., data=Boston)
+summary(lm.fit_all)
+names(lm.fit_all)
+coef(lm.fit_all)
 
-#setup model with only lstat variable
+#Predict median house values (medv) for given values of lstat and rm)
+
+#fit a model with only lstat variable
 lm.fit_2<-lm(medv~lstat, data=Boston)
 summary(lm.fit_2)
 names(lm.fit_2)
 coef(lm.fit_2)
 
-#setup model with only rm variable
+#fit a model with only rm variable
 lm.fit_3<-lm(medv~rm, data=Boston)
 summary(lm.fit_3)
 names(lm.fit_3)
 coef(lm.fit_3)
 
-#Predict median house values (medv) for given values of lstat and rm)
 
 predict(lm.fit_2, data.frame(lstat=c(15, 45)), interval = "confidence")
+#Comment : There is 95% confidence that when lstat=15 medv (median house value) is between
+# 19.7  and 20.9
+
 predict(lm.fit_2, data.frame(lstat=c(15, 45)), interval = "prediction")
+#Comment : 95% prediction interval is between 8.07 and 32.5  for median house value
+#when lstat=15
 
 predict(lm.fit_3, data.frame(rm=c(7, 10)), interval = "confidence" )
 predict(lm.fit_3, data.frame(rm=c(7, 10)), interval = "prediction" )
+
+#********************************************
+#EVALUATE MODEL PERFORMANCE
+#********************************************
+
+#try the multiple linear regression with 2 predictors age and lstat
+lm.fit_m<-lm(medv~lstat+age, data = Boston)
+summary(lm.fit_m)
+
+#All variables
+lm.fit_all<-lm(medv~., data=Boston)
+summary(lm.fit_all)
+
+#Only lstat variable
+lm.fit_lstat<-lm(medv~lstat, data=Boston)
+summary(lm.fit_lstat)
+
+#summary statistics for Rsquared values
+summary(lm.fit_m)$r.sq
+
+#summary for RSE
+summary(lm.fit_m)$sigma
+
+### use vif() to compute variance inflation factors
+### vif is part of car library
+vif(lm.fit_m)
+
+lm.fit_age<-lm(medv~age, data = Boston)
+summary(lm.fit_age)
+
+### in the last regression $age has a high p-value, meaning that age may have an interaction
+#with the error term
+
+### run regression using all predictors and excluding $age
+lm.fit_noage<-lm(medv~.-age, data=Boston)
+summary(lm.fit_noage)
+
+
+### Verify the interaction terms (age and lstat)
+summary(lm(medv ~ lstat*age , data = Boston ))
+#Comment : the interaction between age and lstat does not improve the model(r-squared value is still weak)
+
+
+### We try a non-linear transformation of the predictors
+lm.fit_transformed <- lm(medv ~lstat + I(lstat ^2), data = Boston)
+summary(lm.fit_transformed)
+
+#Comment : ### a near Zero p-value associated with the quadratic term
+### leads to an improved model
+
+## use anova() to further quantify the extent to which quadratic
+### fit is superior to the linear fit
+lm.fit <- lm(medv ~ lstat, data = Boston)
+anova(lm.fit, lm.fit_transformed)
+
+#Comment : ### null hypothesis is that the two models fit data equally well
+### alternative hypothesis is that the full model is superior
+### F-statistic is 135 and p-value ~ 0 is clear evidence 
+### that model containing the predictors lstat and lstat^2
+### is superior
 
 
